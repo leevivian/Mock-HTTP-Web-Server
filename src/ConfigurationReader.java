16a234c226d1964734c91aa46151c7244cb8a7d7
Vivian Lee <vivianlee8295@gmail.com>
2017-02-03 10:22:03 -0800

    Initial commit

---
+++ src/ConfigurationReader.java
@@ -0,0 +1,34 @@
+import java.io.File;
+
+/**
+ * Created by Vivian on 2/3/17.
+ */
+
+public class ConfigurationReader {
+
+    private File file;
+
+    public static void main(String args[]) {
+        System.out.println("Yo");
+    }
+
+    public ConfigurationReader(String filename) {
+
+
+    }
+
+    public boolean hasMoreLines() {
+
+        return false;
+    }
+
+    public String nextLine() {
+
+        return "test";
+    }
+
+    public void load() {
+
+    }
+
+}
