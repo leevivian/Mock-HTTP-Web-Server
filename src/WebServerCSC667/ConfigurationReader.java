52c1417dd01e27cd7a4ce6d9a45745ca16004561
Vivian Lee <vivianlee8295@gmail.com>
2017-02-06 10:26:10 -0800

    Update ConfigurationReader.java, still missing hasMoreLines

--- src/WebServerCSC667/ConfigurationReader.java
+++ src/WebServerCSC667/ConfigurationReader.java
@@ -1,6 +1,6 @@
 package WebServerCSC667;
 
-import java.io.File;
+import java.io.*;
 
 /**
  * Created by Vivian on 2/3/17.
@@ -9,14 +9,14 @@ import java.io.File;
 public class ConfigurationReader {
 
     private File file;
-
-    public static void main(String args[]) {
-        System.out.println("Yo");
-    }
+    private BufferedReader conf;
+    private InputStream inputStream;
+    private String line;
 
     public ConfigurationReader(String filename) {
-
-
+        //Creates a new File instance by converting the given pathname string into an abstract pathname.
+        file = new File(filename);
+        load();
     }
 
     public boolean hasMoreLines() {
@@ -25,12 +25,30 @@ public class ConfigurationReader {
     }
 
     public String nextLine() {
+        try {
+            line = conf.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-        return "test";
+        return line;
     }
 
     public void load() {
+        try {
+            conf = new BufferedReader(new FileReader("conf/" + file));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
+    public static void main(String args[]) {
+        ConfigurationReader test = new ConfigurationReader("httpd.conf");
+        System.out.println(test);
+        System.out.println(test.nextLine());
+        System.out.println(test.nextLine());
+        System.out.println(test.nextLine());
+        System.out.println(test.nextLine());
     }
 
 }
