576977bae61485b1b5f5bbe14c5a65d700dda57b
Vivian Lee <vivianlee8295@gmail.com>
2017-02-05 17:58:20 -0800

    Added config folder, modified Request.java

--- src/WebServerCSC667/Request.java
+++ src/WebServerCSC667/Request.java
@@ -1,25 +1,53 @@
 package WebServerCSC667;
 
 import java.util.Dictionary;
+import java.util.Hashtable;
+import java.util.Map;
+import java.util.stream.Stream;
 
-/**
- * Created by rain2 on 2/3/2017.
- */
 public class Request {
-    private String url;
+    private String uri;
     //private ? body;
     private String verb;
     private String httpVersion;
-    private Dictionary headers;
+    private Dictionary<String, String> headers;
+
+    private String myStr;
 
     public Request (String test){
-
+        myStr = test;
+        parse();
     }
     public Request (Stream client){
 
     }
     public void parse (){
+        String[] arr = myStr.split("\n");
 
+        // HTTP_METHOD IDENTIFIER HTTP_VERSION
+        String[] firstLine = arr[0].split(" ");
+        verb = firstLine[0];
+        uri = firstLine[1];
+        httpVersion = firstLine[2];
+
+        headers = new Hashtable();
+        int index = 1;
+        while (arr[index].contains(": ")) { // or while != " " ?
+            String[] headersArr = arr[index].split(": ");
+            headers.put(headersArr[0], headersArr[1]);
+            index++;
+        }
     }
     //accessors
+    // get something from ConfigurationReader?
+
+    public static void main (String args[]) {
+        Request myReq = new Request("GET /docs/index.html HTTP/1.1\n" +
+                                         "Host: www.nowhere123.com\n" +
+                                         "Accept: image/gif, image/jpeg, */*\n" +
+                                         "Accept-Language: en-us\n" +
+                                         "Accept-Encoding: gzip, deflate\n" +
+                                         "User-Agent: Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1)\n" +
+                                         "    ");
+    }
 }
