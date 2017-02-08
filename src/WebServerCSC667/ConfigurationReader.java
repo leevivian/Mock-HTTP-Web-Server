cea8fd8cb91295559951f7956147a96712669059
raincsc <rain2288@gmail.com>
2017-02-07 22:15:21 -0800

    Modified ConfigurationReader, Revised HttpdConf
    change "File file" to package private variable
    Modified the entire ConfigurationReader class
    Revised HttpdConf to include inheritance

--- src/WebServerCSC667/ConfigurationReader.java
+++ src/WebServerCSC667/ConfigurationReader.java
@@ -1,36 +1,64 @@
 package WebServerCSC667;
 
+
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 /**
- * Created by Vivian on 2/3/17.
+ * Created by rain2 on 2/7/2017.
  */
-
 public class ConfigurationReader {
+    File file;
+    String[] parsedFile;
+    private int parsedFileIndex = 0;
 
-    private File file;
-
-    public static void main(String args[]) {
-        System.out.println("Yo");
+    public ConfigurationReader() {
     }
-
     public ConfigurationReader(String filename) {
-
-
+        //Creates a new File instance by converting the given pathname string into an abstract pathname.
+        file = new File(filename);
+        load();
     }
 
     public boolean hasMoreLines() {
-
-        return false;
+        if (parsedFile.length  > parsedFileIndex){
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 
-    public String nextLine() {
-
-        return "test";
+    public String nextLine(){
+        if (hasMoreLines() == true) {
+            String temp = parsedFile[parsedFileIndex];
+            parsedFileIndex++;
+            System.out.println("nextLine TRUE");
+            return temp;
+        }
+        System.out.println("nextLine FALSE");
+        return null;
     }
 
     public void load() {
+        try {
+            String fileContents = new String(Files.readAllBytes(Paths.get("conf/"+file)));
+            parsedFile = fileContents.split("\n");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
+
+    }
+
+    public static void main(String args[]) {
+        ConfigurationReader test = new ConfigurationReader("httpd.conf");
+        System.out.println("hello");
+        System.out.println(test);
+        System.out.println(test.parsedFile[5]);
+        //System.out.println(test.nextLine());
     }
 
 }
--- src/WebServerCSC667/HttpdConf.java
+++ src/WebServerCSC667/HttpdConf.java
@@ -1,25 +1,39 @@
 package WebServerCSC667;
 
+import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.HashMap;
 
-public class HttpdConf extends ConfigurationReader {
-
-    private HashMap<String, String> aliases = new HashMap<>();
+public class HttpdConf extends ConfigurationReader{
+    //TODO:
+    //CREATE HASH MAP FOR EACH VARIABLE
+    private HashMap <String, String> aliases = new HashMap<>();
     private HashMap <String, String> scriptAliases = new HashMap<>();
-    private String fileLocation;
-    private String fileContents;
-    private String[] parts;
     String[] temp;
     private HashMap <String, String> httpdConfig = new HashMap<>();
+
     // etc.
 
     public HttpdConf(String fileName) {
-        fileLocation = "conf/" + fileName;
+        file = new File (fileName);
         load();
 
-        parts = fileContents.split("\n");
+        do{
+            temp = nextLine().split(" ");
+                if (temp[0].equals("Alias")){
+                    aliases.put(temp[1],temp[2]);
+                }
+                else if (temp[0].equals ("ScriptAlias")) {
+                    scriptAliases.put(temp[1],temp[2]);
+                }
+                else{
+                    httpdConfig.put(temp[0], temp[1]);
+                }
+        }while(hasMoreLines() == true);
+
+
+        /*
         for (int index = 0; index < parts.length; index++){
             temp = parts[index].split(" ");
 
@@ -33,8 +47,9 @@ public class HttpdConf extends ConfigurationReader {
                 httpdConfig.put(temp[0], temp[1]);
             }
         }
+        */
     }
-
+/*
     public void load() {
         try {
              fileContents = new String(Files.readAllBytes(Paths.get(fileLocation)));
@@ -43,14 +58,14 @@ public class HttpdConf extends ConfigurationReader {
             System.out.println("Error: Failed to load file");
         }
     }
-/*
+*/
+
     public static void main(String args[]) {
         HttpdConf test = new HttpdConf("httpd.conf");
-        System.out.println(test.fileContents);
-        System.out.println(test.parts[1]);
-        System.out.println("AAAA\n" + test.aliases);
-        System.out.println("AAAA\n" + test.scriptAliases);
+        System.out.println(test.parsedFile);
+        //System.out.println(test.parts[1]);
+        System.out.println("ALIASES\n" + test.aliases);
+        System.out.println("SCRIPT\n" + test.scriptAliases);
         System.out.println(test.httpdConfig);
     }
-    */
 }
