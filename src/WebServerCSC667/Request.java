522df60b2105d5b0b7be44f8166b192d03912e0a
Vivian Lee <vivianlee8295@gmail.com>
2017-02-11 17:40:16 -0800

    Implemented Server with tests to Request(String req) constructor

--- src/WebServerCSC667/Request.java
+++ src/WebServerCSC667/Request.java
@@ -15,11 +15,11 @@ public class Request {
 
     private Object myStr;
 
-    public Request (String test) throws BadRequest{
+    public Request (String test){
         myStr = test;
         parse();
     }
-    public Request (Stream client) throws BadRequest{
+    public Request (Stream client) {
         String thisLine;
         String streamToString = client.iterator().next().toString();
         StringReader readString = new StringReader(streamToString);
@@ -34,7 +34,7 @@ public class Request {
         }
         parse();
     }
-    public void parse() throws BadRequest{
+    public void parse() {
         boolean contentLength = false;
         String[] arr = (myStr.toString()).split("[\n\r]+");
 
@@ -46,7 +46,7 @@ public class Request {
 
         int index = 1;
         headers = new HashMap();
-        while (arr[index].contains(": ")) { // or while != " " ?
+        while (arr[index].contains(": ") && index < arr.length - 1) { // or while != " " ?
             String[] headersArr = arr[index].split(": ");
             headers.put(headersArr[0], headersArr[1]);
             index++;
@@ -59,18 +59,32 @@ public class Request {
 
     }
 
+    public void printMe() {
+        // For testing purposes
+        System.out.println("-----THIS IS A TEST from the Request class-----");
+        System.out.println("Verb:" + verb);
+        System.out.println("URI:" + uri);
+        System.out.println("Header:" + headers);
+        System.out.println("Body:" + body);
+        System.out.println("-----End of test!-----");
+    }
+
     // accessors
 
-    public static void main (String args[]) throws BadRequest{
-        String test = "GET /docs/index.html HTTP/1.1\n" +
-                "Host: www.nowhere123.com\n" +
-                "Content-Length: 11\n" +
-                "    \n" +
-                "Hello World";
+    // for testing
+    public static void main (String args[]){
+        String test = "POST / HTTP/1.1\n" +
+                "cache-control: no-cache\n" +
+                "Postman-Token: 69936131-e3f1-4e70-9a2f-dbb51d33c814\n" +
+                "User-Agent: PostmanRuntime/3.0.9\n" +
+                "Accept: */*\n" +
+                "Host: localhost:8096\n" +
+                "accept-encoding: gzip, deflate\n" +
+                "content-length: 0\n" +
+                "Connection: keep-alive";
 
         Request myReq = new Request(Stream.of(test));
 
-        System.out.println("Headers: " + myReq.headers + "\nBody: " + myReq.body);
-
     }
+
 }
--- src/WebServerCSC667/Server.java
+++ src/WebServerCSC667/Server.java
@@ -1,25 +1,72 @@
 package WebServerCSC667;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
+import java.net.Socket;
+import java.time.LocalDateTime;
 import java.util.Dictionary;
+import java.lang.Exception;
 
-/**
- * Created by rain2 on 2/3/2017.
- */
+
 public class Server {
     private HttpdConf configuration;
     private MimeTypes mimeTypes;
     private ServerSocket socket;
     private Dictionary accessFiles;
+    private Socket connection;
 
-    public void start(){
+
+    // for testing
+    Request myReq;
+
+    public void start() throws IOException{
         configuration = new HttpdConf("httpd.conf");
         mimeTypes = new MimeTypes( "mime.types");
 
+        socket = new ServerSocket(configuration.getPort());
+
+        while (true) {
+            connection = socket.accept();
+            getRequest(connection);
+            connection.close();
+        }
     }
-    public static void main(String[] args){
-        System.out.print("hello");
+
+    private void getRequest(Socket mySocket) throws IOException {
+        System.out.println("This is an HTTP Request from Postman");
+
+        String currentLine;
+        String completeLine = null;
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(mySocket.getInputStream()));
+
+        while((currentLine = br.readLine()) != null) {
+            System.out.println(currentLine);
+
+            // for Testing Request's Constructor with String
+            completeLine += currentLine + "\n";
+        }
+
+
+        if (completeLine != null) {
+            try {
+                myReq = new Request(completeLine);
+                myReq.printMe();
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+    public static void main(String[] args) throws IOException{
+        System.out.println("hello");
         Server test = new Server();
         test.start();
     }
+
 }
