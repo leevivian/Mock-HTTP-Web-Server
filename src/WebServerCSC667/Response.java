86974ae1e6273e2a512ba7c0d53a340b0523a4c4
rain <rain2288@gmail.com>
2017-02-13 19:36:15 -0800

    First Attempt at Response.java

--- src/WebServerCSC667/Response.java
+++ src/WebServerCSC667/Response.java
@@ -1,19 +1,29 @@
 package WebServerCSC667;
 
+import java.io.IOException;
 import java.io.OutputStream;
+import java.nio.charset.Charset;
 
 /**
  * Created by rain2 on 2/3/2017.
  */
 public class Response {
     int code; // status code
-    String reasonPhrase;
+    String reasonPhrase = "Response test";
     Resource resource;
 
     public Response (Resource resource){
-
+        this.resource = resource;
+        code = resource.getResponseCode();
+        System.out.println(reasonPhrase);
     }
 
-    //public send(OutputStream out){
-    //}
+    //TODO: make send non void
+    public void send(OutputStream out){
+        try {
+            out.write(reasonPhrase.getBytes(Charset.forName("UTF-8")));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
