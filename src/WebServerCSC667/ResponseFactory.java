a573354987dd2dabfa375e8b7e03a6980e42f7df
rain <rain2288@gmail.com>
2017-02-17 18:26:41 -0800

    ResponseFactory.java - PUT completed renamed orignial httpd.conf => ORG.SWITCH.BACK.B4.SUBMIT.httpd.conf created new httpd.conf to use relative server pathing

--- src/WebServerCSC667/ResponseFactory.java
+++ src/WebServerCSC667/ResponseFactory.java
@@ -12,7 +12,7 @@ import java.nio.file.Paths;
 public class ResponseFactory {
 
 
-    public Response getResponse(Request request, Resource resource) {
+    public static Response getResponse(Request request, Resource resource) {
         //TODO: Access checks
         if (resource.isProtected() == true){
             //401 and 403 erros here
@@ -27,13 +27,14 @@ public class ResponseFactory {
                 return new Response(resource,200);
             }
             else if (resource.isScript() == false) {
-
+                File file = new File(resource.getAbsolutePath());
                 switch(request.getVerb()) {
                     //TODO code for each switch case
+
                     case "PUT":
                         //if file already exists
-                        if (new File(resource.getAbsolutePath()).isFile() == false){
-                            return new Response(resource, 404);
+                        if (file.isFile() == true){
+                            return new Response(resource, 400);
                         }
                         //do put
                         else {
@@ -47,8 +48,10 @@ public class ResponseFactory {
                             //Files.write(file, data, StandardOpenOption.APPEND);
                             return new Response(resource, 201);
                         }
+                    //DELETE WORKS
                     case "DELETE":
-                        File file = new File(resource.getAbsolutePath());
+
+                        //System.out.println(resource.getAbsolutePath());
                         if (file.isFile() == true){
                         file.delete();
                         return new Response(resource,204);
@@ -60,8 +63,24 @@ public class ResponseFactory {
                     case "GET":
                         //TODO: returns for POST & HEAD in get
                         return new Response(resource, 200);
+                    case "POST":
+                        if (resource.isModifiedURI() == true){
+                            //TODO: implement body correctly
+                            try {
+                                resource.setBody(Files.readAllLines((Paths.get(resource.getAbsolutePath()))));
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                            return new Response(resource, 200);
+                        }
+                        else return new Response(resource, 304);
+                    case "HEAD":
+                        if (resource.isModifiedURI() == true){
+                            return new Response(resource, 200);
+                        }
+                        else return new Response(resource, 304);
                     default:
-                        return new Response(resource, 200);
+                        return new Response(resource, 400);
                 }
             }
             else {
