24bfdd27850bdf3a983c583ac5a249264b792c2d
rain <rain2288@gmail.com>
2017-02-24 00:19:14 -0800

    updated unique send for headresponse

--- src/WebServerCSC667/response/HeadResponse.java
+++ src/WebServerCSC667/response/HeadResponse.java
@@ -2,6 +2,9 @@ package WebServerCSC667.response;
 
 import WebServerCSC667.Resource;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintStream;
 import java.util.Date;
 
 /**
@@ -10,14 +13,34 @@ import java.util.Date;
 public class HeadResponse extends Response {
     public HeadResponse (Resource resource){
         super(resource, 200);
-        reasonPhrase = "OK";
+        reasonPhrase = "";
         setSendBody(false);
     }
-
+    @Override
     public String getDefaultHeader(){
         return ("Date: " + new Date() +
                 "\nServer: CSC 667 Sailor Scouts" +
                 "\nContent-Type: " + resource.getContentType() +
+                //TODO: cache-control is hard coded
+                "\nCache-Control: private, max-age 86400 " +
+                //EntityTag etag = new EntityTag(Integer.toString(myBook.hashCode()));
                 "\nLast-Modified: " + resource.getLastModified());
+    }
+
+    @Override
+    public void send(OutputStream out){
+        PrintStream ps = new PrintStream(out);
+
+        ps.println(getInitialHeader());
+        ps.println(getDefaultHeader());
+
+        ps.flush();
+        ps.close();
+        try {
+            out.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
