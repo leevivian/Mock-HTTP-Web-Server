6c811f09b04098322b02e08f6c45bc2aff94c6ff
rain <rain2288@gmail.com>
2017-02-25 01:31:31 -0800

    code clean up

--- src/WebServerCSC667/HttpdConf.java
+++ src/WebServerCSC667/HttpdConf.java
@@ -3,11 +3,11 @@ package WebServerCSC667;
 import java.io.File;
 import java.util.HashMap;
 
-public class HttpdConf extends ConfigurationReader{
+public class HttpdConf extends ConfigurationReader {
 
-    private HashMap <String, String> aliases = new HashMap<>();
-    private HashMap <String, String> scriptAliases = new HashMap<>();
-    private HashMap <String, String> httpdConfig = new HashMap<>();
+    private HashMap<String, String> aliases = new HashMap<>();
+    private HashMap<String, String> scriptAliases = new HashMap<>();
+    private HashMap<String, String> httpdConfig = new HashMap<>();
     private String documentRoot = "";
     private String serverRoot = "";
     private String logFileLocaction = "";
@@ -45,7 +45,7 @@ public class HttpdConf extends ConfigurationReader{
     }
 
     public HttpdConf(String fileName) {
-        setFile (new File (fileName));
+        setFile(new File(fileName));
         load();
     }
 
@@ -54,24 +54,19 @@ public class HttpdConf extends ConfigurationReader{
         parse();
     }
 
-    public void parse(){
+    public void parse() {
         String[] temp;
-        while(hasMoreLines()){
-            temp = nextLine().split(" ");
+        while (hasMoreLines()) {
+            temp = nextLine().replaceAll("\"", "").split(" ");
             if (temp.length > 1) {
-                temp[1] = temp[1].replaceAll("\"", "");
-                //System.out.println(temp[1]);
-                //System.out.println(temp[0]);
                 switch (temp[0]) {
                     case "DocumentRoot":
                         documentRoot = temp[1];
                         break;
                     case "Alias":
-                        temp[2] = temp[2].replaceAll("\"", "");
                         aliases.put(temp[1], temp[2]);
                         break;
                     case "ScriptAlias":
-                        temp[2] = temp[2].replaceAll("\"", "");
                         scriptAliases.put(temp[1], temp[2]);
                         break;
                     case "Listen":
@@ -85,34 +80,15 @@ public class HttpdConf extends ConfigurationReader{
                         break;
                     case "AccessFileName":
                         accessFileName = temp[1];
+                        break;
                     case "DirectoryIndex":
                         directoryIndex = temp[1];
+                        break;
                     default:
                         httpdConfig.put(temp[0], temp[1]);
                         break;
                 }
             }
         }
-    }
-
-    @Override
-    public String toString() {
-        return "HttpdConf{" +
-                "\naliases=" + aliases +
-                ", \nscriptAliases=" + scriptAliases +
-                ", \nhttpdConfig=" + httpdConfig +
-                ", \ndocumentRoot='" + documentRoot + '\'' +
-                ", \nserverRoot='" + serverRoot + '\'' +
-                ", \nlogFileLocaction='" + logFileLocaction + '\'' +
-                ", \nport=" + port +
-                '}';
-    }
-    public static void main(String args[]) {
-        HttpdConf test = new HttpdConf("httpd.conf");
-        //System.out.println(test.parts[1]);
-        //System.out.println("ALIASES:  " + test.aliases);
-        //System.out.println("SCRIPT:  " + test.scriptAliases);
-        //System.out.println("port:" + test.port);
-        System.out.println(test.toString());
     }
 }
